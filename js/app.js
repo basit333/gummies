c244640d8e014e8bd374f34a11854375f7d12dab
basit333 <shahbasit741@gmail.com>
2023-03-29 21:50:49 +0500

    made privacy page, terms page

---
+++ js/app.js
@@ -0,0 +1,10 @@
+const btnMale = document.querySelector(".btn-male");
+const btnFemale = document.querySelector(".btn-female");
+
+btnMale.addEventListener("click", function () {
+  console.log("hello");
+});
+
+btnFemale.addEventListener("click", function () {
+  console.log("hello");
+});
